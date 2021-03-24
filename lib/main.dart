8da3347a9b6e2807857f333162074a1706b6dbff
Morn-Mey <Morn-Mey@example.com>
2021-03-24 14:48:20 +0700

    git pull origin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'screens/navigation_bar.dart';
 
 void main() {
@@ -10,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.red,

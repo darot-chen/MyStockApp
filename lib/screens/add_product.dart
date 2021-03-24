15522790bfebfc1c8c28217fa925e4f85e9ce85c
Morn-Mey <Morn-Mey@example.com>
2021-03-24 20:00:28 +0700

    close 3 add product screen

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -6,7 +6,48 @@ class AddProductPage extends StatefulWidget {
   _AddProductPageState createState() => _AddProductPageState();
 }
 
+const MaterialColor _buttonTextColor = MaterialColor(0xFFE53C49, <int, Color>{
+  50: Color(0xFFE53C49),
+  100: Color(0xFFE53C49),
+  200: Color(0xFFE53C49),
+  300: Color(0xFFE53C49),
+  400: Color(0xFFE53C49),
+  500: Color(0xFFE53C49),
+  600: Color(0xFFE53C49),
+  700: Color(0xFFE53C49),
+  800: Color(0xFFE53C49),
+  900: Color(0xFFE53C49),
+});
+
 class _AddProductPageState extends State<AddProductPage> {
+  DateTime _date = DateTime.now();
+  Future<Null> _selectDate(BuildContext context) async {
+    DateTime _datePicker = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(1999),
+      lastDate: DateTime(2050),
+      builder: (BuildContext context, Widget child) {
+        return Theme(
+          data: ThemeData(
+            primarySwatch: _buttonTextColor,
+            primaryColor: Color(0xFFE53C49),
+            accentColor: Color(0xFFE53C49),
+          ),
+          child: child,
+        );
+      },
+    );
+    if (_datePicker != null && _datePicker != _date) {
+      setState(() {
+        _date = _datePicker;
+        print(
+          _date.toString(),
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +62,30 @@ class _AddProductPageState extends State<AddProductPage> {
       child: ListView(
         children: [
           _buildCategory,
-          SizedBox(height: 15,),
-          _buildcat,
+          SizedBox(
+            height: 10,
+          ),
+          _buildProductName,
+          SizedBox(
+            height: 10,
+          ),
+          _buildQuantities,
+          SizedBox(
+            height: 10,
+          ),
+          _buildSizes,
+          SizedBox(
+            height: 10,
+          ),
+          _buildDescript,
+          SizedBox(
+            height: 10,
+          ),
+          _buildProductPhoto,
+          SizedBox(
+            height: 10,
+          ),
+          _buildImportDate,
         ],
       ),
     );
@@ -30,33 +93,387 @@ class _AddProductPageState extends State<AddProductPage> {
 
   get _buildCategory {
     return Container(
-      child: Text(
-        'Category',
-        style: TextStyle(
-          color: Color(0xFF243859),
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Category',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            height: 36,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "Drinks, Snacks, clothes .....",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.arrow_drop_down_circle_rounded,
+                      size: 24,
+                      color: Color(0xFF243859),
+                    ),
+                    onPressed: () {}),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  get _buildcat{
+  get _buildProductName {
     return Container(
-      decoration: BoxDecoration(
-        color: Color(0xFFE1E1E1),
-        borderRadius: BorderRadius.circular(4),
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(4),
-            borderSide: BorderSide(color: Color(0xFFE1E1E1)),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Product Name',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          border: OutlineInputBorder(),
-          hintText: "Drinks, Snacks, clothes .....",
-          hintStyle: TextStyle(color: Color(0xFF000000),),
-        ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            height: 36,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildQuantities {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildQuantity,
+          _buildPrice,
+        ],
+      ),
+    );
+  }
+
+  get _buildQuantity {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Text(
+              'Quantities',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            height: 36,
+            width: 170,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildPrice {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Text(
+              'Price',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            height: 36,
+            width: 90,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildSizes {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildSize,
+          _buildLiftStatus,
+        ],
+      ),
+    );
+  }
+
+  get _buildSize {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Size',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            height: 36,
+            width: 170,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "None",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.arrow_drop_down_circle_rounded,
+                      size: 24,
+                      color: Color(0xFF243859),
+                    ),
+                    onPressed: () {}),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildLiftStatus {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Lift Status',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            height: 36,
+            width: 170,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "None",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.arrow_drop_down_circle_rounded,
+                      size: 24,
+                      color: Color(0xFF243859),
+                    ),
+                    onPressed: () {}),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildDescript {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Descriptions',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildProductPhoto {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Product Photo',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                prefixIcon: IconButton(
+                    icon: Icon(
+                      Icons.add_a_photo_rounded,
+                      size: 25,
+                      color: Color(0xFF243859),
+                    ),
+                    onPressed: () {}),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  get _buildImportDate {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Import Date',
+              style: TextStyle(
+                color: Color(0xFF243859),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 9,
+          ),
+          Container(
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                fillColor: Color(0xFFE1E1E1),
+                filled: true,
+                hintText: "",
+                hintStyle: TextStyle(color: Color(0xFF000000)),
+                prefixIcon: IconButton(
+                    icon: Icon(
+                      Icons.date_range_rounded,
+                      size: 25,
+                      color: Color(0xFF243859),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectDate(context);
+                      });
+                    }),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
